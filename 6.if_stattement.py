93f636bbed7a7885d96c9aa2083d94cec2fdd026
Aqu1bK <aquibkhan7221@gmail.com>
2024-12-21 11:32:36 +0530

    weight conversion

---
+++ 6.if_stattement.py
@@ -0,0 +1,47 @@
+# if =  Do come code only IF some condition is True 
+#       Else do something else
+
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("You are now signed up!")
+# elif age < 0:
+#     print("You haven't been born yet")
+# else:
+#     print("you must be 18+ sign up")
+
+
+# ---------------------------------------------------
+
+
+
+# response = input("Would you like food? (Y/N): ")
+
+# if response  == "Y":
+#    print("Have some food!")
+# else:
+#    print("No food for you!")
+
+
+
+# ----------------------------------------------------
+
+
+
+# name = input("Enter your name: ")
+
+# if name  =="":
+#     print("You did not type in your name!")
+# else:
+#     print(f"Hello {name}")
+
+
+
+# ----------------------------------------------------
+
+online = True
+
+if online:
+    print("The user is online")
+else:
+    print("The user is offline")
